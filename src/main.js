7c4b93b053509576d0c166874f3540a646109691
wangyue <411489690@qq.com>
2019-09-19 17:50:35 +0800

    reset.css添加

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import Vue from 'vue'
 import App from '@/App'
 import router from '@/router'
 import store from '@/store';
-
+import '@/assets/css/reset.css'
 import Cube from 'cube-ui'
 // H5
 import 'amfe-flexible'
